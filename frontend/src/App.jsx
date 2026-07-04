e9254a4abb87e4db37a1c5767c4deea127d29b0e
komal choudhury <choudhuryk363@gmail.com>
2026-07-04 16:57:41 +0530

    updated files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Payroll from "./pages/Payroll";
 import EmployeeProfile from "./pages/EmployeeProfile";
 import Attendance from "./pages/Attendance";
 import LeavePage from "./pages/LeavePage";
@@ -14,7 +17,10 @@ function App() {
         <nav className="navbar">
           <ul className="nav-list">
             <li className="nav-item">
-              <Link to="/" className="nav-link">Employee Profile</Link>
+              <Link to="/dashboard" className="nav-link">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile" className="nav-link">Employee Profile</Link>
             </li>
             <li className="nav-item">
               <Link to="/attendance" className="nav-link">Attendance</Link>
@@ -25,12 +31,22 @@ function App() {
             <li className="nav-item">
               <Link to="/admin-leave" className="nav-link">Admin Leave</Link>
             </li>
+            <li className="nav-item">
+              <Link to="/payroll" className="nav-link">Payroll</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
           </ul>
         </nav>
 
         <div className="container">
           <Routes>
-            <Route path="/" element={<EmployeeProfile />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/payroll" element={<Payroll />} />
+            <Route path="/profile" element={<EmployeeProfile />} />
             <Route path="/attendance" element={<Attendance />} />
             <Route path="/leave" element={<LeavePage />} />
             <Route path="/admin-leave" element={<AdminLeavePage />} />
